e391a5bd3c5c355fb4e1a5b9649ede0b45abc61d
ffayad@ptcdb.edu.ps <ffayad@ptcdb.edu.ps>
2020-09-14 20:30:34 +0300

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,19 @@
   <div class="container">
     <div class="row">
           <div class="col-xs-12">
-            <appQuote>
+            <button @click="selected = 'appQuote'">Quote</button>
+            <button @click="selected = 'appAuthor'">Author</button>
+            <button @click="selected = 'appNew'">New</button>
+            <hr>
+            <p>{{ selected }}</p>
+            <component :is='selected'>
+              <p>Default Content</p>
+            </component>
+            <!-- <appQuote>
               <h2 slot="title">{{ quoteTitle }}</h2>
               <p>a wonderful Quote</p>
-            </appQuote>
+            </appQuote> -->
+            <hr>
           </div>
     </div>   
   </div>
@@ -13,14 +22,20 @@
 
 <script>
 import Quote from './components/Quote';
+import Author from './components/Author';
+import New from './components/New';
+
 export default {
   data: function(){
     return{
-      quoteTitle: 'The Quote'
+      quoteTitle: 'The Quote',
+      selected: 'appQuote'
     }
   },
   components:{
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
  
 }
